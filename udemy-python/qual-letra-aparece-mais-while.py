014aa2bac74fda9076b70793015151139f22eb33
RafaAndrade11 <rafaandrade2810@gmail.com>
2024-10-12 23:00:32 -0300

    Ainda exercitando while, completando calculadora

--- udemy-python/qual-letra-aparece-mais-while.py
+++ udemy-python/qual-letra-aparece-mais-while.py
@@ -3,17 +3,25 @@
 frase = 'O Python é uma linguagem de programação multiparadigma. \
     Python foi criado por Guido van Rossum.'
     
-indice = 1
-letra_anterior = 'O'
+indice = 0
+letra_que_mais_apareceu = ''
+quantidade_apareceu_mais_vezes = 0
 
 while indice < len(frase):
     letra_atual = frase[indice]
+    quantidade_apareceu_mais_vezes_atual = frase.count(letra_atual)
     
-    if (frase.count(letra_atual)) > (frase.count(letra_anterior)):
-        letra_mais_contada = frase[indice]
+    if letra_atual == ' ':
+        indice += 1
+        continue
+   
+    if quantidade_apareceu_mais_vezes < quantidade_apareceu_mais_vezes_atual:
+        quantidade_apareceu_mais_vezes = quantidade_apareceu_mais_vezes_atual
+        letra_que_mais_apareceu = letra_atual
+        
+    if quantidade_apareceu_mais_vezes == quantidade_apareceu_mais_vezes_atual:
+        letras_que_mais_apareceram = '(' + letra_que_mais_apareceu + ') '
     
-    letra_atual += 1
+    indice += 1
     
-print('A letra mais contada foi: {}'.format(letra_mais_contada))
-
-#TO DO
+print('A letra mais contada foi: {}'.format(letras_que_mais_apareceram))
