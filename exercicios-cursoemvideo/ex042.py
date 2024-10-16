5605fc42a0f0ca9ae4e8a840c46d3badf7d42966
RafaAndrade11 <rafaandrade2810@gmail.com>
2024-10-15 23:45:37 -0300

    Exercicio sobre condicionais

--- exercicios-cursoemvideo/ex042.py
+++ exercicios-cursoemvideo/ex042.py
@@ -6,3 +6,19 @@ que tipo de triângulo será formado:
 - isósceles: dois lados iguais
 - escaleno: todos os lados diferentes
 '''
+
+reta1 = float(input('Digite o comprimento da primera reta: '))
+reta2 = float(input('Digite o comprimento da segunda reta: '))
+reta3 = float(input('Digite o comprimento da terceira reta: '))
+
+if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1 + reta2:
+    if reta1 == reta2 and reta2 == reta3:
+        print('Este é um triângulo equilátero!')
+    elif (reta1 == reta2 and reta1 != reta3) or (reta2 == reta3 and reta2 != reta1) or (reta3 == reta1 and reta3 != reta2):
+        print('Este é um triângulo isósceles!')
+    else:
+        print('Este é um triângulo escaleno!')
+else:
+    print('Os segmentos acima não formam um triângulo.')
+    
+    
--- exercicios-cursoemvideo/ex043.py
+++ exercicios-cursoemvideo/ex043.py
@@ -6,3 +6,9 @@ e mostre seu status, e acordo com a tabela abaixo:
 - 25 até 30: sobrepeso
 - 30 até 40: obesidade
 - acima de 40: obesidade mórbida '''
+
+peso = float(input('Digite seu peso(kg): '))
+altura = float(input('Digite sua altura(m)'))
+imc = peso/(altura * altura)
+
+print(imc)
